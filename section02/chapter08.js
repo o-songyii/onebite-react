e8d3af97b7fb97cf16dbc24d7ec21d1d6d3198b5
o-songyii <osongyii@gmail.com>
2024-04-14 21:24:31 +0900

    배열 메서드2. 순회와 탐색

---
+++ section02/chapter08.js
@@ -0,0 +1,51 @@
+// 5가지 요소 순회 및 탐색 메서드
+// 1. forEach
+// 모든 요소를 순회하면서, 각각의 요소에 특정 동작을 수행시키는 메서드
+let arr1 = [1, 2, 3];
+
+arr1.forEach(function (item, idx, arr) {
+    // console.log(idx, item * 2);
+})
+
+let doubledArr = [];
+arr1.forEach((item) => {
+    doubledArr.push(item * 2);
+})
+
+// 2. includes
+// 배열에 특정 요소가 있는지 확인하는 메소드
+let arr2 = [1, 2, 3];
+let isInclude = arr2.includes(3);
+
+// 3. indexOf
+// 특정 요소의 인덱스(위치)를 찾아서 반환하는 메서드
+let arr3 = [1, 2, 3];
+let index = arr3.indexOf(2); //존재하지 않는 값이면 -1 반환
+
+let objectArr = [
+    { name: "오셀리" },
+    { name: "홍길동" }
+]
+
+console.log(objectArr.indexOf({ name: "오셀리" }))
+
+
+console.log(objectArr.findIndex(
+    (item) => item.name === "홍길동"
+))
+
+// 4. findIndex
+// 모든 요소를 순회하면서, 콜백함수를 만족하는 함수
+// 특정 요소의 인덱스(위치)를 반환하는 메서드
+let arr4 = [1, 2, 3];
+const findedIndex = arr4.findIndex((item) => item % 2 !== 0)
+
+// 5. find
+// 모든 요소를 순회하면서 콜백함수를 만족하는 요소를 찾는데, 요소를 그대로 반영
+let arr5 = [
+    { name: "오셀리" },
+    { name: "홍길동" }
+]
+console.log(objectArr.find(
+    (item) => item.name === "홍길동"
+))
