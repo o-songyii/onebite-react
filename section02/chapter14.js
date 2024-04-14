a3265068a9aa3fceda25bb9e55e33f96432b0cf7
o-songyii <osongyii@gmail.com>
2024-04-14 22:52:13 +0900

    비동기 작업 처리하기 3. async&await

---
+++ section02/chapter14.js
@@ -0,0 +1,31 @@
+// async
+// 어떤 함수를 비동기 함수로 만들어주는 키워드
+// 함수가 프로미스를 반환해주는 키워드
+
+async function getData() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({
+                name: "오셀리",
+                id: "sally"
+            })
+        }, 1500);
+    })
+}
+
+//await
+// async 함수 내부에서만 사용이 가능한 키워드
+// 비동기 함수가 다 처리되기를 기다리는 역할
+
+// function printData() {
+//     getData().then((result) => {
+//         console.log(result);
+//     })
+// }
+
+async function printData() {
+    const data = await getData();
+    console.log(data);
+}
+
+printData();
