040b464b757508fd9ed57fb8f5f76cf313e1d270
o-songyii <osongyii@gmail.com>
2024-05-25 20:19:17 +0900

    실습 준비하기

---
+++ section03/src/index.js
@@ -0,0 +1,8 @@
+// import mul, { add, sub } from "./math.js";
+import randomcolor from 'randomcolor';
+
+const color = randomcolor();
+console.log(color);
+
+// console.log(add(1, 2));
+// console.log(mul(2, 4));
