270561aef0deb8df53da335cfcf67fde55b17321
o-songyii <osongyii@gmail.com>
2024-06-04 00:07:40 +0900

    state로 상태 관리하기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,18 +1,14 @@
 import './App.css'
-import Button from "./components/Button";
+import Bulb from "./components/Bulb.jsx";
+import Counter from "./components/Counter.jsx";
 
 function App() {
-    const buttonProps= {
-        text: "메일",
-        color: "red"
-    }
+    //부모 컴포넌트가 리랜더링되면 불필요하게 자식컴포넌트들도 리랜더링되기때문에 컴포넌트 분리 작업이 필요
+
     return (
         <>
-            <Button {...buttonProps}/>
-            <Button text={"카페"}/>
-            <Button text={"블로그"}>
-                <div>자식요소</div>
-            </Button>
+            <Bulb/>
+            <Counter/>
         </>
     )
 }
