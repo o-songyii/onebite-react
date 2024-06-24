41cc220f86d09cfc17d98eacb6ad6cd48ef911bf
o-songyii <osongyii@gmail.com>
2024-06-24 21:23:10 +0900

    State로 사용자 입력 관리하기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,14 +1,11 @@
 import './App.css'
-import Bulb from "./components/Bulb.jsx";
-import Counter from "./components/Counter.jsx";
+import Register from "./components/Register.jsx"
+
 
 function App() {
-    //부모 컴포넌트가 리랜더링되면 불필요하게 자식컴포넌트들도 리랜더링되기때문에 컴포넌트 분리 작업이 필요
-
     return (
         <>
-            <Bulb/>
-            <Counter/>
+            <Register/>
         </>
     )
 }
